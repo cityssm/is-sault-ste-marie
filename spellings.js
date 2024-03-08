aecead72a16ef4aa2dfc835bb33f6a3879d3cfa8
Dan Gowans <dangowans@users.noreply.github.com>
2024-03-08 10:53:12 -0500

    reorganize

--- spellings.js
+++ spellings.js
@@ -1,25 +1,27 @@
 import cartesianProduct from 'just-cartesian-product';
 export const preferredSaultSteMarieSpelling = 'Sault Ste. Marie';
 export const canadaPostSaultSteMarieSpelling = 'S-STE-MARIE';
-const saultSpellings = [
-    'ault',
-    'dault',
-    's',
-    'salt',
-    'salut',
-    'sault',
-    'saulte',
-    'soo',
-    'sult'
-];
-const steSpellings = ['s', 'st', 'ste', 'saint', 'sainte'];
-const marieSpellings = ['m', 'mare', 'marei', 'maire', 'marie', 'mary'];
+const wordSpellings = {
+    sault: [
+        'ault',
+        'dault',
+        's',
+        'salt',
+        'salut',
+        'sault',
+        'saulte',
+        'soo',
+        'sult'
+    ],
+    ste: ['s', 'st', 'ste', 'saint', 'sainte'],
+    marie: ['m', 'mare', 'marei', 'maire', 'marie', 'mary']
+};
 const lowerCaseSaultSteMarieSpellingsList = cartesianProduct([
-    saultSpellings,
-    steSpellings,
-    marieSpellings
+    wordSpellings.sault,
+    wordSpellings.ste,
+    wordSpellings.marie
 ]).map((combination) => {
     return combination.join(' ');
 });
-lowerCaseSaultSteMarieSpellingsList.push('sault stemarie', 'saultstemarie', 'ssm', 'ssmarie');
+lowerCaseSaultSteMarieSpellingsList.push('the sault', 'the soo', 'sault stemarie', 'saultstemarie', 'ssm', 'ssmarie');
 export const lowerCaseSaultSteMarieSpellings = new Set(lowerCaseSaultSteMarieSpellingsList);
--- spellings.ts
+++ spellings.ts
@@ -4,31 +4,33 @@ export const preferredSaultSteMarieSpelling = 'Sault Ste. Marie'
 
 export const canadaPostSaultSteMarieSpelling = 'S-STE-MARIE'
 
-const saultSpellings = [
-  'ault',
-  'dault',
-  's',
-  'salt',
-  'salut',
-  'sault',
-  'saulte',
-  'soo',
-  'sult'
-]
-
-const steSpellings = ['s', 'st', 'ste', 'saint', 'sainte']
-
-const marieSpellings = ['m', 'mare', 'marei', 'maire', 'marie', 'mary']
+const wordSpellings = {
+  sault: [
+    'ault',
+    'dault',
+    's',
+    'salt',
+    'salut',
+    'sault',
+    'saulte',
+    'soo',
+    'sult'
+  ],
+  ste: ['s', 'st', 'ste', 'saint', 'sainte'],
+  marie: ['m', 'mare', 'marei', 'maire', 'marie', 'mary']
+}
 
 const lowerCaseSaultSteMarieSpellingsList = cartesianProduct([
-  saultSpellings,
-  steSpellings,
-  marieSpellings
+  wordSpellings.sault,
+  wordSpellings.ste,
+  wordSpellings.marie
 ]).map((combination) => {
   return combination.join(' ')
 })
 
 lowerCaseSaultSteMarieSpellingsList.push(
+  'the sault',
+  'the soo',
   'sault stemarie',
   'saultstemarie',
   'ssm',
